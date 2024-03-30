1495704c2c454ece40d21275888d6e3e0abdd2af
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-03-30 09:47:13 -0300

    add-accordionFaq

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,42 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  const activeClass = "active";
 
-tabContent[0].classList.add("active");
+  tabContent[0].classList.add(activeClass);
 
-if (tabMenu.length && tabContent.length) {
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("active");
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove(activeClass);
+      });
+      tabContent[index].classList.add(activeClass);
+    }
+
+    tabMenu.forEach((itenMenu, index) => {
+      itenMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("active");
   }
-
-  tabMenu.forEach((itenMenu, index) => {
-    itenMenu.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
 }
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "active";
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
