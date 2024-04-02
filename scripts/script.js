6500a0483089b6c4a2e833248bf72bd2841b6b17
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-04-02 19:25:10 -0300

    add-animationScroll

--- scripts/script.js
+++ scripts/script.js
@@ -65,3 +65,27 @@ function initScrollSmooth() {
   });
 }
 initScrollSmooth();
+
+
+function initAnimationScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.5;
+
+    function animationScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+        if (isSectionVisible) {
+          section.classList.add("active");
+        }
+      });
+    }
+  }
+
+  animationScroll();
+
+  window.addEventListener("scroll", animationScroll);
+}
+initAnimationScroll();
